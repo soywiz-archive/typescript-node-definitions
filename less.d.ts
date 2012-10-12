4dcda256b39bb5b443e8d7f22e7f51278e031b6f
soywiz <soywiz@gmail.com>
2012-10-12 17:31:02 +0200

    - More work on less.d.ts

--- less.d.ts
+++ less.d.ts
@@ -1,15 +1,25 @@
 module "less" {
 	export var version: number[];
+	export class tree {
+		toCSS(options: RenderOptions) : string;
+	}
 	export class Parser {
+		constructor (options?: ParserOptions);
+		parse(text: string, callback: (error : Error, tree : tree) => void);
 	}
 	export class importer {
 	}
-	export class tree {
+
+	interface ParserOptions {
+		paths?: string[];
+		filename?: string;
 	}
+
 	interface RenderOptions {
 		compress?: bool;
 		yuicompress?: bool;
 	}
+
 	export function render(input : string, callback : (err : Error, result: string) => void) : void;
 	export function render(input : string, options : RenderOptions, callback : (err : Error, result: string) => void) : void;
 	export function writeError(ctx, options);
