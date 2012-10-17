8378012ccf10b8574fb7def7abe2738f3f97b35c
soywiz <soywiz@gmail.com>
2012-10-17 18:16:40 +0200

    - Added colors

---
+++ colors.d.ts
@@ -0,0 +1,28 @@
+// https://github.com/Marak/colors.js
+
+// import _colors = module("colors");
+// var colors = require('colors');
+
+module "colors" {
+	export function setTheme(theme: any);
+	export function addSequencer(name: string, callback: Function);
+
+	// none, browser, console
+	export var mode: string;
+}
+
+declare interface String {
+	bold: String;
+	italic: String;
+	underline: String;
+	inverse: String;
+	white: String;
+	grey: String;
+	black: String;
+	blue: String;
+	cyan: String;
+	green: String;
+	magenta: String;
+	red: String;
+	yellow: String;
+}
