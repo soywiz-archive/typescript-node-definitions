1574f69832c99f2650fff2ec8b5cc306ede5d453
soywiz <soywiz@gmail.com>
2012-11-14 00:02:18 +0100

    - Added superagent.d.ts and supertest.d.ts

---
+++ superagent.d.ts
@@ -0,0 +1,32 @@
+﻿///<reference path='node.d.ts' />
+
+declare module "superagent" {
+	import superagent = module('superagent');
+
+	export interface Response {
+		text: string;
+		header: any;
+		type: string;
+		statusCode: number;
+		req: any;
+		res: any;
+	}
+
+	export class Request {
+		auth(user: string, name: string): Request;
+		redirects(count: number): Request;
+		type(type: string): Request;
+		get(url: string): Request;
+		del(url: string): Request;
+		head(url: string): Request;
+		post(url: string): Request;
+		query(object: any): Request;
+		send(object: any): Request;
+		set(key: string, value: any): Request;
+		field(key: string, value: any): Request;
+		attach(key: string, file: any): Request;
+		end(callback: (err: Error, res: Response) => void): Request;
+	}
+
+	export function(url?: string, cb?: (err: Error, res: Response) => void): Request;
+}
---
+++ supertest.d.ts
@@ -0,0 +1,18 @@
+﻿///<reference path='node.d.ts' />
+///<reference path='superagent.d.ts' />
+
+declare module "supertest" {
+	import supertest = module('supertest');
+	import superagent = module('superagent');
+
+	export class TestRequest extends superagent.Request {
+		serverAddress(app: any, path: string): TestRequest;
+		expect(responseCode: number, cb?: (err: Error, res: superagent.Response) => void): TestRequest;
+		expect(responseBody: string, cb?: (err: Error, res: superagent.Response) => void): TestRequest;
+		expect(headerKey: string, headerValue: string, cb?: (err: Error, res: superagent.Response) => void): TestRequest;
+		assert(res: any, cb?: (err: Error, res: superagent.Response) => void);
+	}
+
+	export function(app: any) : TestRequest;
+	export function(url?: string, cb?: (res: superagent.Response) => void) : TestRequest;
+}
