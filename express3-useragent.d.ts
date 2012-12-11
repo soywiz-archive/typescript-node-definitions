07cf263f69e0de79953251c39e1176ddf0b62a7d
soywiz <soywiz@gmail.com>
2012-12-11 05:26:24 +0100

    - added express3-useragent.d.ts

---
+++ express3-useragent.d.ts
@@ -0,0 +1,24 @@
+﻿/// <reference path="node.d.ts" />
+/// <reference path="express3.d.ts" />
+
+declare module "express3" {
+	interface UserAgent {
+		isMobile: bool;
+		isDesktop: bool;
+		Browser: string;
+		Version: string;
+		OS: string;
+		Platform: string;
+		source: string;
+	}
+
+	interface Request {
+		useragent: UserAgent;
+	}
+}
+
+declare module "express-useragent" {
+	export import express3 = module('express3');
+
+	export function express(): (req: express3.Request, res: express3.Response, next?: Function) => void;
+}
