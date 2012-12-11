fc57f905badb90224d137c67048babc78bc17cfb
soywiz <soywiz@gmail.com>
2012-12-11 08:07:22 +0100

    - small addition

--- async.d.ts
+++ async.d.ts
@@ -3,8 +3,9 @@
 declare module "async" {
 	export function map(values: any[], generator: (err: Error, result: any) => void, callback: (err: Error, results: any[]) => void): void;
 	export function filter(values: any[], generator: (err: Error, result: bool) => void, callback: (err: Error, results: any[]) => void): void;
-	export function parallel(funcs: Function[], callback: (err, result) => void);
-	export function series(funcs: Function[]);
+	export function parallel(funcs: any[], callback: (err, result) => void);
+	//export function series(funcs: ((done: () => void) => void)[]);
+	export function series(funcs: any[], callback?: (err, result) => void);
 
 	// ...
 }
