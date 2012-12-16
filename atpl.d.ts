1312d50d1e7b46ef2cc23f8817a9ccc1f366edd5
soywiz <soywiz@gmail.com>
2012-12-16 18:53:44 +0100

    - Added atpl.d.ts

---
+++ atpl.d.ts
@@ -0,0 +1,10 @@
+﻿module "atpl" {
+	export function compile(templateString, options);
+	export function __express(filename, options, callback);
+
+	export function registerExtension(items: any);
+	export function registerTags(items: any);
+	export function registerFunctions(items: any);
+	export function registerFilters(items: any);
+	export function registerTests(items: any);
+}
