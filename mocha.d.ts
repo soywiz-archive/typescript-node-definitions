0ef5d6e6c685c94de9022e3d9f637ba2f5bb5abd
Ben Sudbury <ben@rebas.com.au>
2013-03-28 18:59:59 +1100

    Updated Mocha Definition to include optional error parameter in done: callback Included it.skip, it.only, describe.skip and describe.only Refined BDD definitions to be more DRY

--- mocha.d.ts
+++ mocha.d.ts
@@ -1,33 +1,27 @@
 // BDD
-declare function describe(cb: () => void);
-declare function describe(cb: (done:() => void) => void);
-declare function describe(title: string, cb: () => void);
-declare function describe(title: string, cb: (done:() => void) => void);
+declare function describe(title?: string, cb?: () => void);
 
-declare function it(cb: () => void);
-declare function it(cb: (done:() => void) => void);
-declare function it(title: string, cb: () => void);
-declare function it(title: string, cb: (done:() => void) => void);
+declare function it(title?: string, cb?: (done?:(err? : Error) => void) => void);
 
-declare function before(cb: () => void);
-declare function before(cb: (done:() => void) => void);
-declare function before(title: string, cb: () => void);
-declare function before(title: string, cb: (done:() => void) => void);
+declare function before(title?: string, cb?: (done?:(err? : Error) => void) => void);
 
-declare function after(cb: () => void);
-declare function after(cb: (done:() => void) => void);
-declare function after(title: string, cb: () => void);
-declare function after(title: string, cb: (done:() => void) => void);
+declare function after(title?: string, cb?: (done?:(err? : Error) => void) => void);
 
-declare function beforeEach(cb: () => void);
-declare function beforeEach(cb: (done:() => void) => void);
-declare function beforeEach(title: string, cb: () => void);
-declare function beforeEach(title: string, cb: (done:() => void) => void);
+declare function beforeEach(title?: string, cb?: (done?:(err? : Error) => void) => void);
 
-declare function afterEach(cb: () => void);
-declare function afterEach(cb: (done:() => void) => void);
-declare function afterEach(title: string, cb: () => void);
-declare function afterEach(title: string, cb: (done:() => void) => void);
+declare function afterEach(title?: string, cb?: (done?:(err? : Error) => void) => void);
+
+declare var it : it;
+declare interface it{
+    skip(title?: string, cb?: (done?:(err? : Error) => void) => void);
+    only(title?: string, cb?: (done?:(err? : Error) => void) => void);
+}
+
+declare var describe : describe;
+declare interface describe{
+    skip(title?: string, cb?: (done?:(err? : Error) => void) => void);
+    only(title?: string, cb?: (done?:(err? : Error) => void) => void);
+}
 
 
 // TDD
