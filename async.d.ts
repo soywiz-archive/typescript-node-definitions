cf2c42a94f3df20699ab930ed1adaa6cb0ad4884
midknight41 <chad_macey@hotmail.com>
2013-12-13 10:10:49 +0000

    added compatibility for TypeScript 0.9.5

--- async.d.ts
+++ async.d.ts
@@ -3,7 +3,7 @@
 declare module "async" {
     export function each(values: any[], iterator: (item: any, callback: Function) => void, callback: (err: Error) => void): void;
 	export function map(values: any[], generator: (err: Error, result: any) => void, callback: (err: Error, results: any[]) => void): void;
-	export function filter(values: any[], generator: (err: Error, result: bool) => void, callback: (err: Error, results: any[]) => void): void;
+	export function filter(values: any[], generator: (err: Error, result: boolean) => void, callback: (err: Error, results: any[]) => void): void;
 	export function parallel(funcs: any[], callback: (err, result) => void);
     export function parallel(funcs: any, callback: (err, result : any) => void);
 	//export function series(funcs: ((done: () => void) => void)[]);
