5e7d030fa29cc4fe4566b669dfc4ded2ce142bd8
Dasa Paddock <dpaddock@esri.com>
2014-01-05 18:29:19 -0800

    Type the result callback parameter as “any” on the parallel, series and waterfall functions.

--- async.d.ts
+++ async.d.ts
@@ -4,9 +4,9 @@ declare module "async" {
     export function each(values: any[], iterator: (item: any, callback: Function) => void, callback: (err: Error) => void): void;
     export function map(values: any[], generator: (err: Error, result: any) => void, callback: (err: Error, results: any[]) => void): void;
     export function filter(values: any[], generator: (err: Error, result: boolean) => void, callback: (err: Error, results: any[]) => void): void;
-    export function parallel(funcs: any[], callback?: (err, result) => void);
-    export function parallel(funcs: any, callback?: (err, result : any) => void);
+    export function parallel(funcs: any[], callback?: (err, result: any) => void);
+    export function parallel(funcs: any, callback?: (err, result: any) => void);
     //export function series(funcs: ((done: () => void) => void)[]);
-    export function series(funcs: any[], callback?: (err, result) => void);
-    export function waterfall(funcs: any[], callback?: (err, result) => void);
+    export function series(funcs: any[], callback?: (err, result: any) => void);
+    export function waterfall(funcs: any[], callback?: (err, result: any) => void);
 }
