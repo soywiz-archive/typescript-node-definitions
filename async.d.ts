c258a386cdf0f6ecd5c328cd42c6901d2ca7a875
Dasa Paddock <dpaddock@esri.com>
2014-01-07 12:52:06 -0800

    Add missing “parallelLimit” functions.

--- async.d.ts
+++ async.d.ts
@@ -6,6 +6,8 @@ declare module "async" {
     export function filter(values: any[], generator: (err: Error, result: boolean) => void, callback: (err: Error, results: any[]) => void): void;
     export function parallel(funcs: any[], callback?: (err, result: any) => void): void;
     export function parallel(funcs: any, callback?: (err, result: any) => void): void;
+    export function parallelLimit(funcs: any[], limit: number, callback?: (err, result: any) => void): void;
+    export function parallelLimit(funcs: any, limit: number, callback?: (err, result: any) => void): void;
     //export function series(funcs: ((done: () => void) => void)[]);
     export function series(funcs: any[], callback?: (err, result: any) => void): void;
     export function waterfall(funcs: any[], callback?: (err, result: any) => void): void;
